deed9d927d82e83ca0ea16b5e79b818d3f49c4da
Dana-Arabasi <danaarabasi@gmail.com>
2026-02-08 15:27:42 +0200

    first commit

---
+++ ex1.py
@@ -0,0 +1,7 @@
+#Exercise: write python script to ask the user for their name and print a greeting.
+name = input("Enter your name: ")
+print("Hello", name)
+
+
+#Run a “Hello World” Python script and upload to GitHub
+print("Hello World")
